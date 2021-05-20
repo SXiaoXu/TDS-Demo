9b4fe20eea2e464d02cdf253f6b40083b60bbd00
xsui <284898541@qq.com>
2021-05-20 15:48:49 +0800

    init

---
+++ TDSDemo-iOS/TDSDemo-iOS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TDSDemo-iOS
+//
+//  Created by Vivian on 2021/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TDSDemo-iOS/TDSDemo-iOS/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TDSDemo-iOS
+//
+//  Created by Vivian on 2021/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
